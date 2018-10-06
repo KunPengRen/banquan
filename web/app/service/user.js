81679b50152fdce42544f8068599d1e49eb33495
Dayu <ren675523481@gmail.com>
2018-10-06 22:12:34 +0800

    user api update

--- web/app/service/user.js
+++ web/app/service/user.js
@@ -20,7 +20,18 @@ class UserService extends Service{
     async addUser(user){
         console.log(user);
         var address=user['address'],name=user['name'],username=user['username'],phone=user['phone'],mail=user['mail'],identify=user['identify'];
-        this.ctx.model.User.create({address,name,username,phone,mail,identify});
+        var userdata = await this.ctx.model.User.findOne({
+            where:{
+                address:address
+            }
+        })
+        if(userdata){
+            console.log(userdata);
+            await userdata.update({address,name,username,phone,mail,identify});
+        }
+        else{
+            await this.ctx.model.User.create({address,name,username,phone,mail,identify});
+        }
         return {"res":true};
     }
 };
